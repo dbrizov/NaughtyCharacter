185608606c2f03e35b9f1451dcbcabaf183b5b6a
Denis Rizov <d.b.rizov@gmail.com>
2014-07-05 23:24:51 +0300

    ThirdPersonController Script added

---
+++ Assets/Scripts/ThirdPersonController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Third person controller.
+/// Processes player input and sends the input as "Move Vector" to the ThirdPersonMotor.
+/// </summary>
+public class ThirdPersonController : MonoBehaviour
+{
+    public static CharacterController CharacterController;
+    public static ThirdPersonController Instance;
+
+    private void Awake()
+    {
+        CharacterController = this.GetComponent<CharacterController>();
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        if (Camera.main == null)
+        {
+            return;
+        }
+
+        this.GetLocomotionInput();
+    }
+
+    private void GetLocomotionInput()
+    {
+
+    }
+}
