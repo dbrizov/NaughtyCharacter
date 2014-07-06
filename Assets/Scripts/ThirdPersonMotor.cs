cfa5bfa80996053bb76bd031fd5b23f1221e568d
Denis Rizov <d.b.rizov@gmail.com>
2014-07-06 09:43:13 +0300

    ThirdPersonMotor basic functionality implemented

--- Assets/Scripts/ThirdPersonMotor.cs
+++ Assets/Scripts/ThirdPersonMotor.cs
@@ -38,11 +38,37 @@ public class ThirdPersonMotor : MonoBehaviour
 
     private void SnapAlignCharacterWithCamera()
     {
-
+        if (this.IsCharacterMoving())
+        {
+            // Rotate the character to match the direction the camera is facing
+            this.transform.rotation = Quaternion.Euler(
+                this.transform.eulerAngles.x, Camera.main.transform.eulerAngles.y, this.transform.eulerAngles.z);
+        }
     }
 
     private void ProcessMotion()
     {
+        // Transform MoveVector to World Space relative to character's orientation
+        this.MoveVector = this.transform.TransformDirection(this.MoveVector);
 
+        // Normalize MoveVector if Magnitude > 1
+        if (this.MoveVector.magnitude > 1)
+        {
+            this.MoveVector.Normalize();
+        }
+
+        // Multiply MoveVector by moveSpeed
+        this.MoveVector *= this.moveSpeed;
+
+        // Convert from units/frame to units/second
+        this.MoveVector *= Time.deltaTime;
+
+        // Move the character
+        ThirdPersonController.CharacterController.Move(this.MoveVector);
+    }
+
+    private bool IsCharacterMoving()
+    {
+        return (this.MoveVector.x != 0) || (this.MoveVector.z != 0);
     }
 }
