06b62178404cf339646d991ed5de8fb917bca0c2
Denis Rizov <d.b.rizov@gmail.com>
2018-12-31 16:03:18 +0200

    Deleted old stuff. Will be starting from scratch

---
+++ Assets/NaughtyCharacter/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private int _targetFrameRate = 60;
+
+    [SerializeField]
+    private float _slowMotion = 0.2f;
+
+    protected virtual void Awake()
+    {
+        Application.targetFrameRate = _targetFrameRate;
+    }
+
+    protected virtual void Update()
+    {
+        if (Input.GetButtonDown("Slow Motion"))
+        {
+            this.ToggleSlowMotion();
+        }
+    }
+
+    private void ToggleSlowMotion()
+    {
+        Time.timeScale = Time.timeScale == 1f ? _slowMotion : 1f;
+    }
+}
