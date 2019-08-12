b37897fd2fc0e4ad414cc21a53f4f79e3966cc40
Denis Rizov <d.b.rizov@gmail.com>
2019-08-12 20:19:02 +0300

    All scripts are now with tabs instead of spaces

--- Assets/NaughtyCharacter/Scripts/PlayerCamera.cs
+++ Assets/NaughtyCharacter/Scripts/PlayerCamera.cs
@@ -2,40 +2,40 @@ using UnityEngine;
 
 namespace NaughtyCharacter
 {
-    public class PlayerCamera : MonoBehaviour
-    {
-        [Tooltip("How fast the camera rotates around the pivot. Value <= 0 are interpreted as instant rotation")]
-        public float RotationSpeed = 0.0f;
-        public float PositionSmoothDamp = 0.0f;
-        public Transform Rig; // The root transform of the camera rig
-        public Transform Pivot; // The point at which the camera pivots around
-        public Camera Camera;
+	public class PlayerCamera : MonoBehaviour
+	{
+		[Tooltip("How fast the camera rotates around the pivot. Value <= 0 are interpreted as instant rotation")]
+		public float RotationSpeed = 0.0f;
+		public float PositionSmoothDamp = 0.0f;
+		public Transform Rig; // The root transform of the camera rig
+		public Transform Pivot; // The point at which the camera pivots around
+		public Camera Camera;
 
-        private Vector3 _cameraVelocity;
+		private Vector3 _cameraVelocity;
 
-        public void SetPosition(Vector3 position)
-        {
-            Rig.position = Vector3.SmoothDamp(Rig.position, position, ref _cameraVelocity, PositionSmoothDamp);
-        }
+		public void SetPosition(Vector3 position)
+		{
+			Rig.position = Vector3.SmoothDamp(Rig.position, position, ref _cameraVelocity, PositionSmoothDamp);
+		}
 
-        public void SetControlRotation(Vector2 controlRotation)
-        {
-            // Y Rotation (Yaw Rotation)
-            Quaternion rigTargetLocalRotation = Quaternion.Euler(0.0f, controlRotation.y, 0.0f);
+		public void SetControlRotation(Vector2 controlRotation)
+		{
+			// Y Rotation (Yaw Rotation)
+			Quaternion rigTargetLocalRotation = Quaternion.Euler(0.0f, controlRotation.y, 0.0f);
 
-            // X Rotation (Pitch Rotation)
-            Quaternion pivotTargetLocalRotation = Quaternion.Euler(controlRotation.x, 0.0f, 0.0f);
+			// X Rotation (Pitch Rotation)
+			Quaternion pivotTargetLocalRotation = Quaternion.Euler(controlRotation.x, 0.0f, 0.0f);
 
-            if (RotationSpeed > 0.0f)
-            {
-                Rig.localRotation = Quaternion.Slerp(Rig.localRotation, rigTargetLocalRotation, RotationSpeed * Time.deltaTime);
-                Pivot.localRotation = Quaternion.Slerp(Pivot.localRotation, pivotTargetLocalRotation, RotationSpeed * Time.deltaTime);
-            }
-            else
-            {
-                Rig.localRotation = rigTargetLocalRotation;
-                Pivot.localRotation = pivotTargetLocalRotation;
-            }
-        }
-    }
+			if (RotationSpeed > 0.0f)
+			{
+				Rig.localRotation = Quaternion.Slerp(Rig.localRotation, rigTargetLocalRotation, RotationSpeed * Time.deltaTime);
+				Pivot.localRotation = Quaternion.Slerp(Pivot.localRotation, pivotTargetLocalRotation, RotationSpeed * Time.deltaTime);
+			}
+			else
+			{
+				Rig.localRotation = rigTargetLocalRotation;
+				Pivot.localRotation = pivotTargetLocalRotation;
+			}
+		}
+	}
 }
--- Assets/NaughtyCharacter/Scripts/TestHelper.cs
+++ Assets/NaughtyCharacter/Scripts/TestHelper.cs
@@ -2,30 +2,30 @@ using UnityEngine;
 
 namespace NaughtyCharacter
 {
-    public class TestHelper : MonoBehaviour
-    {
-        [SerializeField]
-        private int _targetFrameRate = 60;
+	public class TestHelper : MonoBehaviour
+	{
+		[SerializeField]
+		private int _targetFrameRate = 60;
 
-        [SerializeField]
-        private float _slowMotion = 0.1f;
+		[SerializeField]
+		private float _slowMotion = 0.1f;
 
-        protected virtual void Awake()
-        {
-            Application.targetFrameRate = _targetFrameRate;
-        }
+		protected virtual void Awake()
+		{
+			Application.targetFrameRate = _targetFrameRate;
+		}
 
-        protected virtual void Update()
-        {
-            if (Input.GetButtonDown("Slow Motion"))
-            {
-                this.ToggleSlowMotion();
-            }
-        }
+		protected virtual void Update()
+		{
+			if (Input.GetButtonDown("Slow Motion"))
+			{
+				this.ToggleSlowMotion();
+			}
+		}
 
-        private void ToggleSlowMotion()
-        {
-            Time.timeScale = Time.timeScale == 1f ? _slowMotion : 1f;
-        }
-    }
+		private void ToggleSlowMotion()
+		{
+			Time.timeScale = Time.timeScale == 1f ? _slowMotion : 1f;
+		}
+	}
 }
